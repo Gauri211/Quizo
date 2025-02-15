ed6f03e4b7fd3766302f82ab398d2f09942c3859
GBhosle <gaurib49@gmail.com>
2025-02-15 23:25:25 +0530

    Database updated

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -3,12 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  connectionLimit: 10,
-});
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+const pool = mysql.createPool(urlDB);
 
 export default pool.promise();
